b20ef53c00db3ea29b68e63b871cb1bbc61c8428
Maricela Potoc <maricelapotoc@gmail.com>
2023-09-28 12:38:20 +0300

    work

--- work/src/intr_pkg.sv
+++
@@ -1,7 +0,0 @@
-package intr_pkg;
-    `include "uvm_macros.svh"
-    import uvm_pkg::*;
-    `include "afvip_interrupt_sequence_item.svh"
-    `include "afvip_interrupt_monitor.svh"
-    `include "afvip_interrupt_agent.svh"  
-endpackage
